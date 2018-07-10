a1881bc7c07d59ad00e2b1c7dc72cf32703b1f4b
Nikolay Nesvitiy <nikolaynesvitiy@gmail.com>
2018-07-11 00:46:42 +0300

    hs_project_first commit

--- e2e/pages/basePage.po.ts
+++ e2e/pages/basePage.po.ts
@@ -3,5 +3,8 @@ import { browser } from 'protractor';
 export class BasePage {
     goToHomePage () {
         browser.get('/');
+    };
+    waitLoadingElementsOnPages () {
+        browser.sleep(2000);
     }
 }

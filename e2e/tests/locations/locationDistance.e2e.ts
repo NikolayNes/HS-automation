88030046319f74457c4dacdb0980e5c730fd989f
Nikolay Nesvitiy <nikolaynesvitiy@gmail.com>
2018-07-14 21:24:06 +0300

    feature(test): searh location

--- e2e/tests/locations/locationDistance.e2e.ts
+++ e2e/tests/locations/locationDistance.e2e.ts
@@ -2,23 +2,18 @@ import {browser} from "protractor";
 import {LocationsPage} from "../../pages/locationsPage.po";
 import set = Reflect.set;
 
-describe("", () => {
+describe("Go to location page. Search locations and compare current location", () => {
     let locationsPage = new LocationsPage();
     let EC = browser.ExpectedConditions;
 
-    fit("Compare distance", () => {
-
+    it("Go to location page. Search locations", () => {
         locationsPage.navigateToLocationPage();
-        //browser.actions().mouseMove(locationsPage.sliderBtn).mouseMove({x: -158, y: 0}).doubleClick().perform();
         locationsPage.searchLocationsInput.clear();
         locationsPage.searchLocationsInput.sendKeys("New York");
-        browser.wait(EC.visibilityOf(locationsPage.listOfResult.first()), 2000);
         locationsPage.listOfResult.first().click();
-        //browser.actions().mouseMove(locationsPage.sliderBtn).mouseMove({x: 400, y: 0}).perform();
-        browser.actions().mouseMove(locationsPage.sliderBtn).mouseMove({x: 470, y: 0}).doubleClick().perform();
-        locationsPage.findStoresBtn.click();
-        locationsPage.pickFromMap.click();
-        browser.sleep(2000);
-        //browser.actions().mouseMove(locationsPage.sliderBtn).mouseMove({x: 470, y: 0}).doubleClick().perform();
     });
+    it ("Select Spa. Checking distance", () => {
+
+    });
+
 });
